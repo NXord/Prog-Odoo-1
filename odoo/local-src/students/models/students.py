588f0338da85888aeeabc40075efa998ffc948d9
NXord <noe.rolland03@gmail.com>
2023-03-15 17:24:52 +0100

    Code Average, Surcharge de Name_get pour Student

--- odoo/local-src/students/models/students.py
+++ odoo/local-src/students/models/students.py
@@ -14,11 +14,12 @@ class StudentsTraining(models.Model):
 class StudentsStudent(models.Model):
     _name = "students.student"
     _description = "Student table"
-    _rec_name = "number"
+    #_rec_name = "number"
 
     number = fields.Char("Student number", size=11, required=True)
     firstname = fields.Char("Student firstname", size=64, required=True)
     lastname = fields.Char("Student lastname", size=64, required=True)
+    average = fields.Float("Student mark average",compute="compute_average")
     training_id = fields.Many2one(
         string="Training",
         comodel_name="students.training",
@@ -29,6 +30,27 @@ class StudentsStudent(models.Model):
         comodel_name="students.mark",
         inverse_name="student_id",
     )
+
+    @api.onchange(marks_ids)
+    def compute_average(self):
+        for record in self:
+            if record.marks_ids:
+                tCoef=0
+                tMark=0
+                for mark in record.marks_ids:
+                    tMark+=mark.weighted
+                    tCoef+= int(mark.coefficient)
+                if tCoef!=0:
+                    record.average=tMark/tCoef
+            else:
+                record.average = 0
+
+    def name_get(self):
+        result = []
+        for record in self:
+            name = record.firstname + ' ' + record.lastname
+            result.append((record.id, name))
+        return result
 
 class StudentsMark(models.Model):
     _name = "students.mark"
